1b08b4a92b4806221b9a99532827079b4942f806
javad <javad@adorable-island.aeza.network>
2024-01-12 18:38:44 +0100

    Just a test for github

--- upload.py
+++ upload.py
@@ -4,6 +4,7 @@ import telegram
 
 async def main():
     bot = telegram.Bot("6821047694:AAHpK7nIUrQW-53ifVtPwBaQikGaHM4s_dg")
+
     async with bot:
         await bot.send_message(text='hi', chat_id=5368372729)
 
