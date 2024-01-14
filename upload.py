72e9f685da612e810388e70e7a092b10921f2b8c
javad <javad@adorable-island.aeza.network>
2024-01-14 22:25:30 +0100

    this commit will make the chat better and add /help,/clear_history command

--- upload.py
+++ upload.py
@@ -20,8 +20,11 @@ logging.basicConfig(
 )
 
 async def start(update, context):
-    await update.message.reply_text("Enter your username.")
+    await update.message.reply_text("Enter your username")
     return USERNAME
+
+async def helpbot(update, contex):
+    await update.message.reply_text("/start -> Login\n/clear_history -> Start new chat and clear history\n/help -> To see this page again")
 
 async def username(update, contex):
     global usern
@@ -33,7 +36,7 @@ async def password(update, contex):
     global passn
     passn = update.message.text
     if usern in alluser and passn in allpass:
-        await update.message.reply_text('Logged in succesfully!\nAsk me anything you want or just send me a file')
+        await update.message.reply_text('Logged in succesfully!\nAsk me anything you want or just send me a file(use /help for all commands)')
         return LOGGEDIN
     else:
         await update.message.reply_text('Access denied!\nTry again by using /start')
@@ -57,6 +60,12 @@ async def reply_to_message(update: Update, context: CallbackContext):
 
     await update.message.reply_text(response.choices[0].message['content'])
 
+async def clear_history(update, contex):
+    user_id = update.effective_user.id
+    user_chat_history[user_id] = []
+    await update.message.reply_text("Chat history cleared.")
+
+
 async def uploader(update: Update, context ):
     if update.message.video:
         video_file = await context.bot.get_file(update.message.video.file_id)
@@ -79,7 +88,12 @@ if __name__ == '__main__':
         states={
             USERNAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, username)],
             PASSWORD: [MessageHandler(filters.TEXT & ~filters.COMMAND, password)],
-            LOGGEDIN: [MessageHandler(filters.TEXT & (~filters.COMMAND), reply_to_message), MessageHandler(filters.Document.ALL | filters.VIDEO, uploader)],
+            LOGGEDIN: [
+                MessageHandler(filters.TEXT & (~filters.COMMAND), reply_to_message),
+                MessageHandler(filters.Document.ALL | filters.VIDEO, uploader),
+                CommandHandler('clear_history',clear_history),
+                CommandHandler('help',helpbot)
+            ],
         },
         fallbacks=[],
         allow_reentry=True
