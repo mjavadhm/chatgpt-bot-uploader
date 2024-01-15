0068a62d4f14cb816086aeea3332503623a73400
javad <javad@adorable-island.aeza.network>
2024-01-15 01:05:33 +0100

    bot V 1.00

--- upload.py
+++ upload.py
@@ -6,12 +6,14 @@ from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, InlineQueryHandler, CallbackContext, ConversationHandler
 
 user_chat_history = {}
+user_chat_setting = {}
 alluser = ['admin','javad','Javad','Admin']
 allpass = ['1234','0150','Admin']
-USERNAME, PASSWORD, LOGGEDIN = range(3)
+USERNAME, PASSWORD, LOGGEDIN, WAIT_FOR_USER_MESSAGE = range(4)
 
 
-openai.api_key = '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'
+
+openai.api_key = '******'
 
 
 logging.basicConfig(
@@ -20,11 +22,18 @@ logging.basicConfig(
 )
 
 async def start(update, context):
+    user_id = update.effective_user.id
+    if user_id not in user_chat_history:
+        user_chat_setting[user_id] = "you are a Telegram bot designed to assist users in solving problems. You are helpful."
+
     await update.message.reply_text("Enter your username")
     return USERNAME
 
-async def helpbot(update, contex):
-    await update.message.reply_text("/start -> Login\n/clear_history -> Start new chat and clear history\n/help -> To see this page again")
+async def about(update, context):
+    await update.message.reply_text('Another chatgpt bot but! youcan choose role and save your files\n(🤥We respect your privacy🤥)\nyou can see list of the commands in /commands\nGithub link : https://github.com/mjavadhm/juploadbot/')
+
+async def commandsa(update, contex):
+    await update.message.reply_text("/start ---> Login\n/clear_history ---> Start new chat and clear history\n/set_role <input> ---> set bot role\n/reset_role ---> default setting for bot\n/about ---> More information about Bot")
 
 async def username(update, contex):
     global usern
@@ -36,11 +45,29 @@ async def password(update, contex):
     global passn
     passn = update.message.text
     if usern in alluser and passn in allpass:
-        await update.message.reply_text('Logged in succesfully!\nAsk me anything you want or just send me a file(use /help for all commands)')
+        await update.message.reply_text('Logged in succesfully!\nAsk me anything you want or just send me a file(use /about for more information)')
         return LOGGEDIN
     else:
         await update.message.reply_text('Access denied!\nTry again by using /start')
         return ConversationHandler.END
+
+async def role(update, context):
+    user_id = update.effective_user.id
+    await update.message.reply_text(f'Your role has been set to: {user_chat_setting[user_id]}')
+
+async def set_role(update, context):
+    await update.message.reply_text('Enter bot role:')
+    return WAIT_FOR_USER_MESSAGE
+
+async def get_user_message(update, context):
+    user_id = update.effective_user.id
+    user_chat_setting[user_id] = update.message.text
+    await update.message.reply_text(f'Your role has been set to: {user_chat_setting[user_id]}')
+    return LOGGEDIN
+
+async def reset_role(update, contex):
+    user_id = update.effective_user.id
+    user_chat_setting[user_id] = "you are a Telegram bot designed to assist users in solving problems. You are helpful."
 
 async def reply_to_message(update: Update, context: CallbackContext):
     user_input = update.message.text
@@ -48,12 +75,14 @@ async def reply_to_message(update: Update, context: CallbackContext):
 
     if user_id not in user_chat_history:
         user_chat_history[user_id] = []
+#    if user_id not in user_chat_history:
+#       user_chat_setting[user_id] = "you are a Telegram bot designed to assist users in solving problems. You are helpful."
 
     user_chat_history[user_id].append({"role": "user", "content": user_input})
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-16k",
-        messages=[{"role": "system", "content": "you are a Telegram bot designed to assist users in solving problems. You have a helpful."}, *user_chat_history[user_id]],
+        messages=[{"role": "system", "content": user_chat_setting[user_id] }, *user_chat_history[user_id]],
         n=1,
         stop=None
     )
@@ -83,16 +112,28 @@ async def uploader(update: Update, context ):
         
 if __name__ == '__main__':
     application = ApplicationBuilder().token('6594664984:AAFQtIMLpdppT2xFeaGmi7Tb0djSx4qVpQc').build()
+    
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            USERNAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, username)],
-            PASSWORD: [MessageHandler(filters.TEXT & ~filters.COMMAND, password)],
+            USERNAME: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, username)
+            ],
+            PASSWORD: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, password)
+            ],
             LOGGEDIN: [
                 MessageHandler(filters.TEXT & (~filters.COMMAND), reply_to_message),
                 MessageHandler(filters.Document.ALL | filters.VIDEO, uploader),
                 CommandHandler('clear_history',clear_history),
-                CommandHandler('help',helpbot)
+                CommandHandler('about',about),
+                CommandHandler('commands',commandsa),
+                CommandHandler('role',role),
+                CommandHandler('set_role', set_role),
+                CommandHandler('reset_role', reset_role)
+            ],
+            WAIT_FOR_USER_MESSAGE: [
+                MessageHandler(filters.TEXT, get_user_message)
             ],
         },
         fallbacks=[],
